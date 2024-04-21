846f97284345836346b9a8fda964e54b74849d0b
Sidney Wackenier <Sidneywackenier@hotmail.com>
2024-04-21 19:51:20 +0000

    Filter function added

--- deelopdracht/index.ts
+++ deelopdracht/index.ts
@@ -14,10 +14,26 @@ app.set("port", 3000);
 let data : Character[];
 
 app.get("/", async (req, res) => {
-      let response = await fetch("https://raw.githubusercontent.com/AP-G-1PRO-Webontwikkeling/project-webontwikkeling-SidneyWackenier/main/deelopdracht/json/dc.json");
-      data = await response.json();
-      res.render("index", { data });
+  let response = await fetch("https://raw.githubusercontent.com/AP-G-1PRO-Webontwikkeling/project-webontwikkeling-SidneyWackenier/main/deelopdracht/json/dc.json");
+  data = await response.json();
+
+  let q: string = req.query.q as string ?? "";
+
+  let filteredCharacters: Character[] = data.filter((character) => {
+      return character.name.toLowerCase().includes(q.toLowerCase());
+  });
+
+console.log('Query parameter q:', q);
+console.log('Filtered characters:', filteredCharacters);
+
+
+  res.render("index", {
+      data: data,
+      filteredCharacters: filteredCharacters,
+      q: q 
+  });
 });
+
 
 app.listen(app.get("port"), () => {
   console.log("[server] http://localhost:" + app.get("port"));
