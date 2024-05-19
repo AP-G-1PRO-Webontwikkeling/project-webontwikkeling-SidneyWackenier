aba0e78fdc9c233ba584a69400c757a059050872
Sidney Wackenier <Sidneywackenier@hotmail.com>
2024-05-19 05:41:58 +0000

    Minor changes + sorting function in progress

--- deelopdracht/index.ts
+++ deelopdracht/index.ts
@@ -19,20 +19,35 @@ app.get("/", async (req, res) => {
   let response = await fetch("https://raw.githubusercontent.com/AP-G-1PRO-Webontwikkeling/project-webontwikkeling-SidneyWackenier/main/deelopdracht/json/dc.json");
   data = await response.json();
 
-  let q: string = req.query.q as string ?? "";
-
+  // Filter-functie
+  let q = req.query.q as string ?? "";
   let filteredCharacters: Character[] = data.filter((character) => {
-      return character.name.toLowerCase().includes(q.toLowerCase());
+    return character.name.toLowerCase().includes(q.toLowerCase());
   });
 
-console.log('Query parameter q:', q);
-console.log('Filtered characters:', filteredCharacters);
+  // Sorteer-functie
+  const sortField = typeof req.query.sortField === "string" ? req.query.sortField : "name";
+  const sortDirection = typeof req.query.sortDirection === "string" ? req.query.sortDirection : "asc";
+
+  let sortedCards = [...filteredCharacters].sort((a, b) => {
+    if (sortField === "name") {
+      return sortDirection === "asc" ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name);
+    } else if (sortField === "cost") {
+      return sortDirection === "asc" ? a.cost - b.cost : b.cost - a.cost;
+    } else if (sortField === "power") {
+      return sortDirection === "asc" ? a.power - b.power : b.power - a.power;  
+    } else {
+      return 0;
+    }
+  });
+
+  console.log('Sorted cards:', sortedCards);
 
 
   res.render("index", {
-      data: data,
-      filteredCharacters: filteredCharacters,
-      q: q 
+    data: filteredCharacters,
+    filteredCharacters: sortedCards,
+    q: q 
   });
 });
 
